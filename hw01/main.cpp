48205b78dd2cb0184566b9ceed05ac72803dd496
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2022-03-05 19:18:06 +0100

    hw01: implement identicalFiles

--- hw01/main.cpp
+++ hw01/main.cpp
@@ -1,8 +1,24 @@
+#include <fstream>
+
 #include "huffman.cpp"
 
 bool identicalFiles(const char *fileName1, const char *fileName2) {
-    // todo
-    return false;
+    std::ifstream file1(fileName1, std::ios::binary);
+    std::ifstream file2(fileName2, std::ios::binary);
+
+    char charFile1, charFile2;
+    while (file1.good() && file2.good()) {
+        charFile1 = file1.get();
+        charFile2 = file2.get();
+
+        if (charFile1 != charFile2)
+            return false;
+    }
+
+    if (file1.good() != file2.good())
+        return false;
+
+    return true;
 }
 
 int main(void) {
