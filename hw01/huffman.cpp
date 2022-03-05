ab9a0e0e270fbff176bf35718e2673945bfc36c7
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2022-03-05 19:17:19 +0100

    hw01: add description & tests

---
+++ hw01/huffman.cpp
@@ -0,0 +1,34 @@
+#ifndef __PROGTEST__
+
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+#endif /* __PROGTEST__ */
+
+bool decompressFile(const char *inFileName, const char *outFileName) {
+    // todo
+    return false;
+}
+
+bool compressFile(const char *inFileName, const char *outFileName) {
+    // keep this dummy implementation (no bonus) or implement the compression
+    // (bonus)
+    return false;
+}
