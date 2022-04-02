b6e686871f2598d02750b153d41d30def7df64d0
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2022-04-02 16:40:50 +0200

    hw04: implement write atd.

--- hw04/file.cpp
+++ hw04/file.cpp
@@ -7,7 +7,7 @@
 #include <cstring>
 #include <iostream>
 
-#endif /* __PROGTEST__ */
+#endif  // __PROGTEST__
 
 class CFile {
     template <typename T>
@@ -18,15 +18,26 @@ class CFile {
         uint32_t capacity;
         T* data = nullptr;
 
-        Vector(uint32_t reserve = 256) : capacity(reserve), data(new T[sizeof(T) * capacity]) {
+        Vector(uint32_t reserve = 16) : capacity(reserve), data(new T[sizeof(T) * capacity]) {
         }
 
-        Vector(const Vector<T>& vector) {
-            // todo
+        Vector(const Vector<T>& vector) : length(vector.length), capacity(vector.capacity), data(new T[sizeof(T) * vector.capacity]) {
+            memcpy(data, vector.data, sizeof(T) * vector.length);
         }
 
         ~Vector() {
             delete[] data;
+        }
+
+        Vector<T>& operator=(const Vector<T>& vector) {
+            if (&vector != this) {
+                length = vector.length;
+                capacity = vector.capacity;
+                clear(capacity);
+                memcpy(data, vector.data, sizeof(T) * vector.length);
+            }
+
+            return *this;
         }
 
         void push(const T& value) {
@@ -36,25 +47,55 @@ class CFile {
             data[length++] = value;
         }
 
-        void pushBytes(const uint8_t* source, uint32_t bytes) {
-            while (length + bytes / sizeof(T) > capacity)
+        void insertArray(const T* source, uint32_t count, uint32_t index) {
+            while (index + count > capacity)
                 grow();
 
-            memcpy(&data[length], source, bytes);
-            length += bytes / sizeof(T);
+            memcpy(&data[index], source, count);
+            length = std::max(index + count, length);
         }
 
-        void shrink() {
-            capacity = length;
+        T pop() {
+            length--;
+            return data[length];
+        }
+
+        void resize(uint32_t newCapacity) {
+            if (capacity == newCapacity)
+                return;
+
+            capacity = newCapacity;
+            length = std::min(capacity, length);
             T* newData = new T[sizeof(T) * capacity];
             memcpy(newData, data, sizeof(T) * length);
             delete[] data;
             data = newData;
         }
 
+        Vector<T>* attach() {
+            refCount++;
+            return this;
+        }
+
+        void detach() {
+            if (--refCount == 0)
+                delete this;
+        }
+
+        void clear(uint32_t newCapacity = 16) {
+            length = 0;
+            capacity = newCapacity;
+            delete[] data;
+            data = new T[sizeof(T) * capacity];
+        }
+
     private:
         void grow() {
-            capacity = 32 + 2 * capacity;
+            if (capacity == 0)
+                capacity = 16;
+            else
+                capacity = 2 * capacity;
+
             T* newData = new T[sizeof(T) * capacity];
             memcpy(newData, data, sizeof(T) * length);
             delete[] data;
@@ -62,37 +103,47 @@ class CFile {
         }
     };
 
+    struct DataPositonPair {
+        Vector<uint8_t>* data;
+        uint32_t position;
+    };
+
     Vector<uint8_t>* _data = nullptr;
     uint32_t _position = 0;
-    Vector<Vector<uint8_t>*> _history;
+    Vector<DataPositonPair> _history;
 
 public:
     CFile() : _data(new Vector<uint8_t>()), _history(16) {
     }
 
-    CFile(const CFile& file) {
-        // todo
+    CFile(const CFile& file) : _data(file._data->attach()), _position(file._position), _history(file._history) {
+        for (uint32_t i = 0; i < _history.length; i++)
+            _history.data[i].data->attach();
     }
 
     ~CFile() {
-        if (--_data->refCount == 0)
-            delete _data;
-
-        for (int i = 0; i < _history.length; i++) {
-            if (--_history.data[i]->refCount == 0)
-                delete _history.data[i];
-        }
+        _clear();
     }
 
     CFile& operator=(const CFile& file) {
-        // todo
+        if (&file != this) {
+            _clear();
+
+            _data = file._data->attach();
+            _position = file._position;
+            _history = file._history;
+            for (uint32_t i = 0; i < _history.length; i++)
+                _history.data[i].data->attach();
+        }
+
+        return *this;
     }
 
-    bool seek(uint32_t offset) {
-        if (_position + offset < 0 || _position + offset > _data->length)
+    bool seek(uint32_t pos) {
+        if (pos > _data->length)
             return false;
 
-        _position += offset;
+        _position = pos;
         return true;
     }
 
@@ -105,13 +156,22 @@ public:
     }
 
     uint32_t write(const uint8_t* source, uint32_t bytes) {
+        if (bytes == 0)
+            return bytes;
+
         _detachData();
-        // todo
+        _data->insertArray(source, bytes, _position);
+        _position += bytes;
+
+        return bytes;
     }
 
     void truncate() {
+        if (_position == _data->capacity)
+            return;
+
         _detachData();
-        _data->shrink();
+        _data->resize(_position);
     }
 
     uint32_t fileSize() const {
@@ -119,18 +179,36 @@ public:
     }
 
     void addVersion() {
-        _data->refCount++;
-        _history.push(_data);
+        _history.push({_data->attach(), _position});
     }
 
     bool undoVersion() {
-        if (--_history.data[i]->refCount == 0)
-                delete _history.data[i];
+        if (_history.length == 0)
+            return false;
+
+        _data->detach();
+        auto version = _history.pop();
+        _data = version.data;
+        _position = version.position;
+
+        return true;
     }
 
 private:
     void _detachData() {
-        if (_data->refCount > 1)
+        if (_data->refCount > 1) {
+            _data->detach();
             _data = new Vector<uint8_t>(*_data);
+        }
+    }
+
+    void _clear() {
+        _data->detach();
+        _data = nullptr;
+
+        for (uint32_t i = 0; i < _history.length; i++)
+            _history.data[i].data->detach();
+
+        _history.clear();
     }
 };
