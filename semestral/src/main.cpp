bd5b03aa3ea62d235de1ad2e78aa5345128f05ea
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2022-04-08 02:33:54 +0200

    sem: add description, make, doxygen

---
+++ semestral/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/**
+ * @brief program entry point
+ * 
+ * :)
+ *
+ * @param[in] argc argument count
+ * @param[in] argv argument array
+ *
+ * @return exit code
+ */
+int main(int argc, char **argv) {
+    std::cout << "hello world!" << std::endl;
+    return EXIT_SUCCESS;
+}
