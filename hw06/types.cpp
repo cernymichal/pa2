eb0edf1702b9553e6530ede3d0cc4f93ff588f60
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2022-04-10 02:46:31 +0200

    hw06: add description and tests

---
+++ hw06/types.cpp
@@ -0,0 +1,50 @@
+#ifndef __PROGTEST__
+
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#endif  // __PROGTEST__
+
+class CDataTypeInt {
+    // todo
+};
+
+class CDataTypeDouble {
+    // todo
+};
+
+class CDataTypeEnum {
+    // todo
+};
+
+class CDataTypeStruct {
+    // todo
+};
+
+class CDataTypeArray {
+    // todo
+};
+
+class CDataTypePtr {
+    // todo
+};
